d359afa833df8c9a4322f87bffab56256b450d1e
renaner123 <renaner123@gmail.com>
2025-02-18 22:52:40 -0300

    estrutura inicial do projeto com CRUD básico

---
+++ src/main/java/br/com/nlw/events/controllers/EventController.java
@@ -0,0 +1,37 @@
+package br.com.nlw.events.controllers;
+
+import br.com.nlw.events.model.Event;
+import br.com.nlw.events.service.EventService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class EventController {
+
+    @Autowired
+    private EventService eventService;
+
+    @PostMapping("/events")
+    public Event addNewEvent(@RequestBody Event event) {
+        return eventService.addNewEvent(event);
+    }
+
+    @GetMapping("/events")
+    public List<Event> getAllEvents() {
+        return eventService.getAllEvents();
+    }
+
+    @GetMapping("/events/{prettyName}")
+    public ResponseEntity<Event> getByPrettyName(@PathVariable String prettyName) {
+        Event event = eventService.getByPrettyName(prettyName);
+
+        if (event == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok().body(event);
+    }
+}
